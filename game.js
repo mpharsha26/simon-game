73b8119e4f533df2f3cf430ab119da79afa6fd55
MP <mpharsha@MSI>
2020-09-01 02:32:43 +0530

    add js changes

--- game.js
+++ game.js
@@ -1,17 +1,39 @@
 var gameStart = false;
-
-if (gameStart == false) {
-    $(document).keypress(function (event){
-        console.log(event.key);
-        $("h1").text('Level 1');
-        makeSound('blue');
+var levelNo = 1;
+var colors = ['green', 'red', 'yellow', 'blue'];
+var seq = [];
+$(document).keypress(function(event){
+    $('h1').text('Level ' + levelNo);
+    getSeq(levelNo);
+    $.each(seq, function (i, num){
+        $("." + colors[num]).delay(i * 100).addClass('pressed', 100).removeClass('pressed', 100, function(){
+            makeSound(colors[num]);
+        });
     });
-    gameStart = true;
+    // seq.forEach(function(num){
+    //     $("." + colors[num]).addClass('pressed', 100).removeClass('pressed', 100, function(){
+    //         makeSound(colors[num]);
+    //     });
+    //     // $("#" + colors[num]).addClass("pressed");
+    //     // setTimeout(function () {
+    //     //     $("#" + colors[num]).removeClass("pressed");
+    //     // }, 100);
+    //     //makeSound(colors[num]);
+    // });
+    levelNo += 1;
+});
+   
+function gameOver(){
+
 }
 
+function getSeq(levelNo){
+    var randomNo = Math.floor(Math.random() * 4);
+    seq.push(randomNo);
+}
 
 function makeSound(color){
     var sound = new Audio('sounds/' + color + '.mp3');
     sound.play();
-};
+}
 

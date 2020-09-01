e519387001eae62ac19d06bfb3405e2c6fe8d934
MP <codermp.26@gmail.com>
2020-09-01 18:27:39 +0530

    add gameover func

--- game.js
+++ game.js
@@ -2,44 +2,60 @@ var gameStart = false;
 var levelNo = 1;
 var colors = ['green', 'red', 'yellow', 'blue'];
 var seq = [];
+var userSeq = [];
+var noOfClicks;
+
 $(document).keypress(function(event){
     $('h1').text('Level ' + levelNo);
     getSeq(levelNo);
-    var i = 0;                 
-    function myLoop() {         
-        setTimeout(function() {   
-            console.log('hey ' + i, seq, colors[seq[i]]); 
-            $("." + colors[seq[i]]).addClass('pressed', 100).removeClass('pressed', 100);
-            makeSound(colors[seq[i]]);
-            i++;                   
-            if (i < seq.length)          
-                myLoop();                                 
-        }, 400);
-    }
-    myLoop()
-    
-    // $(seq).each(function (i, num){
-    //     console.log(i);
-    //     $("." + colors[num]).addClass('pressed', 100).removeClass('pressed', 100, function(){
-    //         makeSound(colors[num]);
-    //      });
-    //     });
-
-    // seq.forEach(function(num){
-    //     $("." + colors[num]).addClass('pressed', 100).removeClass('pressed', 100, function(){
-    //         makeSound(colors[num]);
-    //     });
-    //     // $("#" + colors[num]).addClass("pressed");
-    //     // setTimeout(function () {
-    //     //     $("#" + colors[num]).removeClass("pressed");
-    //     // }, 100);
-    //     //makeSound(colors[num]);
-    // });
-    levelNo += 1;
-});
+    var i = 0;
+    noOfClicks = 0;                 
    
-function gameOver(){
+    displayAndPlaySequence(i)  // displaying the game sequence and playing the respective sounds
+    
+    levelNo += 1;
+    
+});
 
+$('.btn').click(function(){
+    //console.log($(this).attr('id'), colors[seq[seq.length - 1]]);
+    noOfClicks++;
+    var userClickedButton = $(this).attr('id')
+    if (userClickedButton == colors[seq[noOfClicks - 1]]){
+        console.log('working', noOfClicks);
+        $(this).addClass('pressed', 100).removeClass('pressed', 50, function (){
+            makeSound(userClickedButton);
+        });
+        // animateButton($(this));
+    }
+    else {
+        console.log(userClickedButton, noOfClicks);
+        gameOver();
+    }
+});
+
+function displayAndPlaySequence(i) {         
+    setTimeout(function() {   
+        $("." + colors[seq[i]]).addClass('pressed', 100).removeClass('pressed', 100, function(){
+            makeSound(colors[seq[i]]);
+            i++;
+            if (i < seq.length)          
+            displayAndPlaySequence(i);
+        });                                              
+    }, 300);
+}
+
+// function animateButton(button){
+//     $(button).addClass('pressed', 100).removeClass('pressed', 100, function (){
+//         makeSound(button.attr('id'));
+//     });
+// }
+
+function gameOver(){
+    var sound = new Audio('sounds/wrong.mp3');
+    sound.play();
+    $('h1').text('Game over 😩 Press any key to restart');
+    $('body').addClass('gameover', 10).removeClass('gameover', 500);
 }
 
 function getSeq(levelNo){

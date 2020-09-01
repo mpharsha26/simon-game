3de3d3189124a4def97e4cc75a314eec11bcd6aa
MP <codermp.26@gmail.com>
2020-09-01 16:56:24 +0530

    add recursive loops for delay between iterations

--- game.js
+++ game.js
@@ -5,11 +5,26 @@ var seq = [];
 $(document).keypress(function(event){
     $('h1').text('Level ' + levelNo);
     getSeq(levelNo);
-    $.each(seq, function (i, num){
-        $("." + colors[num]).delay(i * 100).addClass('pressed', 100).removeClass('pressed', 100, function(){
-            makeSound(colors[num]);
-        });
-    });
+    var i = 0;                 
+    function myLoop() {         
+        setTimeout(function() {   
+            console.log('hey ' + i, seq, colors[seq[i]]); 
+            $("." + colors[seq[i]]).addClass('pressed', 100).removeClass('pressed', 100);
+            makeSound(colors[seq[i]]);
+            i++;                   
+            if (i < seq.length)          
+                myLoop();                                 
+        }, 400);
+    }
+    myLoop()
+    
+    // $(seq).each(function (i, num){
+    //     console.log(i);
+    //     $("." + colors[num]).addClass('pressed', 100).removeClass('pressed', 100, function(){
+    //         makeSound(colors[num]);
+    //      });
+    //     });
+
     // seq.forEach(function(num){
     //     $("." + colors[num]).addClass('pressed', 100).removeClass('pressed', 100, function(){
     //         makeSound(colors[num]);
